bfc0fa025e8392e050999c9e9aae409620d533da
bifidokk <bifidokk@gmail.com>
2025-03-15 14:24:43 +0200

    Add naive approach

---
+++ naive-approach.py
@@ -0,0 +1,26 @@
+import os
+import openai
+from pathlib import Path
+from dotenv import load_dotenv
+
+from llama_index.core import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+)
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")  
+
+def main():
+    documents = SimpleDirectoryReader("annual-reports").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+
+    query_engine = index.as_query_engine()
+    response = query_engine.query("You are CFO-GPT. Quickly answer which company has more liquidity and how much. Use annual reports. Don't make up information if you're not certain")
+    print(response)
+
+if __name__ == "__main__":
+    main()
+
